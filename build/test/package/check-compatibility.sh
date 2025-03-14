0e1d3400e1fc76715312d4b835718147bc4fc079
Jonathan Rayback <jrayback@gmail.com>
2025-03-14 08:11:29 -0600

    Fix npm packaging problems (#56)
    * update packaging strategy for better esm/browser compatibility
    
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>
    
    * 0.0.6-dev.4
    
    ---------
    
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- build/test/package/check-compatibility.sh
+++ build/test/package/check-compatibility.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Store the base directory of the script dynamically, so we can navigate to the root of the project whenever needed
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 runNodeTest() {
     printf "\n\e[34m***********\n"
     printf "* Node.js *\n"
     printf "***********\e[0m\n\n"
     
-    cd "build/test/package/node" || { echo "Directory not found"; exit 1; }
+    cd "$BASE_DIR/node" || { echo "Directory not found"; exit 1; }
     rm -rf node_modules
     npm run node-test || { echo "Node test failed"; exit 1; }
 }
+
+# Ensure we always start from the base directory
+cd "$BASE_DIR" || { echo "Failed to navigate to base directory"; exit 1; }
 
 echo "Checking compatibility..."
 runNodeTest
